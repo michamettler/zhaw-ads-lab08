58e3bd003c07d71154100f75656ca6ceb71d9774
michamettler <mettlermicha@gmail.com>
2023-11-07 17:09:59 +0100

    finished task 2

--- src/ch/zhaw/ads/LabyrinthServer.java
+++ src/ch/zhaw/ads/LabyrinthServer.java
@@ -1,10 +1,26 @@
 package ch.zhaw.ads;
 
+import java.util.Scanner;
+
 public class LabyrinthServer implements CommandExecutor {
     ServerGraphics g = new ServerGraphics();
 
     public Graph<DijkstraNode, Edge> createGraph(String s) {
-        // TODO implement 8.2
+        Graph<DijkstraNode, Edge> graph = new AdjListGraph<>(DijkstraNode.class, Edge.class);
+        Scanner scanner = new Scanner(s);
+        while (scanner.hasNextLine()) {
+            String[] coordinates = scanner.nextLine().split(" ");
+            try {
+                graph.addNode(coordinates[0]);
+                graph.addNode(coordinates[1]);
+                graph.findNode(coordinates[1]).setPrev(graph.findNode(coordinates[0]));
+                graph.addEdge(coordinates[0], coordinates[1], 0.0);
+                graph.addEdge(coordinates[1], coordinates[0], 0.0);
+            } catch (Throwable e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return graph;
     }
 
     public void drawLabyrinth(Graph<DijkstraNode, Edge> graph) {
@@ -12,7 +28,7 @@ public class LabyrinthServer implements CommandExecutor {
     }
 
     private boolean search(DijkstraNode current, DijkstraNode ziel) {
-        // TODO implement 8.4
+        return false;// TODO implement 8.4
     }
 
     // search and draw result
